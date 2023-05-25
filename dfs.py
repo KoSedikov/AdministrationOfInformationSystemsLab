5458061f348db292a1327773768148d6a83fdcc8
kostyanich Sedikov <kostyasedikov@gmail.com>
2023-05-25 17:58:00 +0300

    добавление комментариев

--- dfs.py
+++ dfs.py
@@ -1,6 +1,7 @@
 
 # Алгоритм поиска в глубину на Python
 
+# Сам алгоритм dfs
 def dfs(graph, begin, visited=None):
     if visited is None:
         visited = set()
@@ -9,15 +10,15 @@ def dfs(graph, begin, visited=None):
     print(begin)
     i = 0
     for next in graph[begin] - visited:
-        try:
+        try: # обработка исключений
             dfs(graph, next, visited)
-            i +=1
+            i +=1 # Подсчет проходов.
         except: print('none Value Exception')
     return visited, i
 
-
+# наш граф
 graph = {'0': set(['4', '2']),
          '1': set(['1', '3']),
          '3': set(['2', '4'])}
 
-dfs(graph, '0')
+dfs(graph, '0') # Вызов функции (для проверки)
